912487a159412ee870e9e95b8c6458bfe44fee0b
Kothapalli <shakotha@gmail.com>
2019-07-03 22:32:05 -0700

    Initial Commit

---
+++ HomeFed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeFed
+//
+//  Created by Kothapalli on 7/3/19.
+//  Copyright © 2019 SAV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
